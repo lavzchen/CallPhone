8772a2b000943b3b328cb9a1b79751518ca26398
chowchow <paoding08@163.com>
2015-03-14 15:10:36 +0800

    Initial Commit

---
+++ CallPhone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CallPhone
+//
+//  Created by chhowchow on 15-3-14.
+//  Copyright (c) 2015年 com.hoperun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CallPhone/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CallPhone
+//
+//  Created by chhowchow on 15-3-14.
+//  Copyright (c) 2015年 com.hoperun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
